c67b7c1f6e5eafdd3985c4c1c3a19f3f5c65872e
dani-CO-CN <23180005+dani-CO-CN@users.noreply.github.com>
2020-10-15 15:12:46 +0200

    Create bock.sh

---
+++ bock.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Time to block Moday through Friday from 8:00-18:00
+begin=$(date --date="8:00" +%s)
+end=$(date --date="18:00" +%s)
+now=$(date +%s)
+day_of_week=$(date +%u)
+
+hosts_to_block=("reddit.com" "orf.at" "facebook.com" "youtube.com" "ycombinator.com" "derstandard.at")
+
+for i in "${hosts_to_block[@]}"
+do
+   if ! grep -q "$i" /etc/hosts
+   then
+        #echo "$i not found, add to file"
+        echo "127.0.0.1 *$i www.$i" >> /etc/hosts  
+    fi
+done
+
+
+if [ "$begin" -le "$now" -a "$now" -le "$end" -a "$day_of_week" -lt 6 ]  ## time to block
+then
+    #echo "block"
+    sed -i "s/^#127.0.0.1/127.0.0.1/g" /etc/hosts
+else
+    #echo "unblock"
+    sed -i "s/^127.0.0.1/#127.0.0.1/g" /etc/hosts
+fi
